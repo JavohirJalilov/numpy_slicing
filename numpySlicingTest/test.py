ff6843f9ad3555562988ad212aa0993527146fb0
JavohirJalilov <javohirjaliloff@gmail.com>
2023-02-14 23:21:07 +0500

    added test chess

--- numpySlicingTest/test.py
+++ numpySlicingTest/test.py
@@ -420,6 +420,59 @@ class PatternSeven:
             print("❌ Failed")
         print(response.status_code)
 
+# test chess
+# ones_array[::2,::2] = 2
+# ones_array[1::2,1::2] = 2
+class Chess:
+        
+    def get_shape(self, solution):
+        return solution.shape
+
+    def fill_ones(self, n, m):
+        arr = np.ones(shape=(n, m),dtype=np.uint8)
+        return arr
+
+    def test_case_1(self, solution):
+        n, m = self.get_shape(solution)
+        expected_arr = self.fill_ones(n, m)
+        expected_arr[::2,::2] = 2
+        expected_arr[1::2,1::2] = 2
+        return np.equal(expected_arr, solution).all()
+
+    def test_case_2(self, solution):
+        return solution.__class__ == np.ndarray
+
+    def check(self, solution, github_username):
+        """
+        Chack if the solution is correct
+
+        Args:
+            solution (np.ndarray): solution to check
+            github_username (str): github username
+
+        Returns:
+            bool: True if the solution is correct
+        """
+        case1 = self.test_case_1(solution)
+        case2 = self.test_case_2(solution)
+
+        isSolved = case1 and case2
+
+        data = {
+            'github': github_username,
+            'repo': 'AIGroup/numpy_slicing',
+            'tasks': [
+                {'isSolved': bool(isSolved), 'name': 'chess'}]
+            }
+        response = requests.post(url, json=data)
+        
+        if isSolved:
+            # done emoji
+            print("✅ Accepted")
+        else:
+            # fail emoji
+            print("❌ Failed")
+        print(response.status_code)
 q0 = FillOnes()
 q1 = PatternOne()
 q2 = PatternTwo()
@@ -428,3 +481,4 @@ q4 = PatternFour()
 q5 = PatternFive()
 q6 = PatternSix()
 q7 = PatternSeven()
+q8 = Chess()
